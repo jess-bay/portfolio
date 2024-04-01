25d3cb38c193afe2ced8e4adccb7c3e392885af3
Jess Bayman <justjessb@gmail.com>
2024-04-02 09:38:13 +1300

    Edit home and about me component

--- client/router.tsx
+++ client/router.tsx
@@ -9,11 +9,13 @@ import Home from './components/Home'
 import { Education } from './components/Education'
 import Skills from './components/Skills'
 import WorkHistory from './components/WorkHistory'
+import AboutMe from './components/AboutMe'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
+      <Route path="/about" element={<AboutMe />} />
       <Route path="/education" element={<Education />} />
       <Route path="/skills" element={<Skills />} />
       <Route path="/workhistory" element={<WorkHistory />} />
