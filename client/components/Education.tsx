bb3789f1d979a9b73632a353812ded751449614a
Jess Bayman <justjessb@gmail.com>
2024-04-05 16:41:14 +1300

    Add styling to change bg, make divs opaque

--- client/components/Education.tsx
+++ client/components/Education.tsx
@@ -6,7 +6,7 @@ export function Education() {
 
   return (
     <>
-      <div className="mt-8">
+      <div className="mt-8 bg-cover bg-center bg-opacity-50">
         <h3 className="text-2xl font-bold mb-4 ml-6">Education</h3>
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4">
           {data?.map((education: EducationType) => (
--- client/components/WorkHistory.tsx
+++ client/components/WorkHistory.tsx
@@ -6,18 +6,23 @@ export default function WorkHistory() {
 
   return (
     <>
-      <div className="mt-8">
-        <h3 className="text-2xl font-bold mb-4 ml-6">Work History</h3>
+      <div className="mt-8 mb-8 ">
+        {/* backdrop-filter bg-white backdrop-blur-lg bg-opacity-50 */}
+        <h3 className="text-2xl font-bold mb-4 ml-6 text-white text-center">
+          Work History
+        </h3>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           {data?.map((work: History) => (
             <div
               key={work.id}
-              className="p-6 mx-6 bg-white border border-gray-300 rounded-md shadow-md transition-transform duration-300 transform hover:scale-105"
+              className="p-6 mx-6 border border-white rounded-md shadow-md transition-transform duration-300 transform hover:scale-105 bg-purple-700 backdrop-blur-lg bg-opacity-80"
             >
-              <p className="text-lg font-semibold">Year: {work.years}</p>
-              <p className="text-gray-600">Company: {work.name}</p>
-              <p className="text-gray-600">Title: {work.title}</p>
-              <p className="text-gray-600">Description: {work.description}</p>
+              <p className="text-lg font-semibold text-white">
+                Year: {work.years}
+              </p>
+              <p className="text-white">Company: {work.name}</p>
+              <p className="text-white">Title: {work.title}</p>
+              <p className="text-white">Description: {work.description}</p>
             </div>
           ))}
         </div>
