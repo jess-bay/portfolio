9e703db1cfc4131779af395149827bfce5398e1d
Jess Bayman <justjessb@gmail.com>
2024-04-22 12:40:08 +1200

    Add project for aim trainer and update work history

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -3,12 +3,83 @@ interface ProjectProps {
 }
 export default function Projects({ darkTheme }: ProjectProps) {
   return (
-    <div id="projects" className="flex justify-center items-center h-screen">
+    <div
+      id="projects"
+      className={`mt-8 mb-8 ${darkTheme ? 'light-theme-bg' : 'dark-theme-bg'}`}
+    >
       <h3
         className={`text-2xl font-bold mb-4 ml-6 mr-6 ${darkTheme ? 'dark-theme-text' : 'light-theme-text'} flex justify-center items-center h-screen`}
       >
         Projects
       </h3>
+      <div className="grid grid-cols-1 gap-8">
+        <div
+          key="aim-trainer"
+          className={`p-6 mb-4 ml-6 mr-6 border border-gray-300 rounded-md shadow-md transition-transform duration-300 transform hover:scale-105 ${
+            darkTheme ? 'bg-purple-800' : 'bg-white'
+          }`}
+          style={{
+            backgroundColor: darkTheme
+              ? 'rgba(128, 0, 128, 0.5)'
+              : 'rgba(255, 255, 255, 0.5)',
+          }}
+        >
+          <p className="test-lg font-semibold">Aim Trainer</p>
+          <br />
+          <p
+            className={`text-gray-600 ${darkTheme ? 'text-white' : 'text-gray-400'}`}
+          >
+            Aim Trainer was the final group project completed at Dev Academy in
+            April 2024. A group of 6 came together from across NZ and worked
+            solely online to deliver a game that serves one main goal: to help
+            gamers improve their mouse handling.
+          </p>
+          <p
+            className={`text-gray-600 ${darkTheme ? 'text-white' : 'text-gray-400'}`}
+          >
+            It became a bonus that this game is suitable for anyone of all ages
+            looking to improve their mouse handling skills, too!
+          </p>
+          <a
+            href="https://aim-trainer.pushed.nz/"
+            target="_blank"
+            rel="noreferrer"
+            className={`text-gray-600 ${darkTheme ? 'text-white' : 'text-gray-400'}`}
+          >
+            <br />
+            Click here to view Aim Trainer!
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
+
+// <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
+//             {data?.map((education: EducationType) => (
+//               <div
+//                 key={education.id}
+//                 className={`p-6 mb-4 ml-6 mr-6 border border-gray-300 rounded-md shadow-md transition-transform duration-300 transform hover:scale-105 ${
+//                   darkTheme ? 'bg-purple-800' : 'bg-white'
+//                 }`}
+//                 style={{
+//                   backgroundColor: darkTheme
+//                     ? 'rgba(128, 0, 128, 0.5)'
+//                     : 'rgba(255, 255, 255, 0.5)',
+//                   // padding: '10px',
+//                 }}
+//               >
+//                 <p className="test-lg font-semibold">
+//                   {education.qualification}
+//                 </p>
+//                 <p
+//                   className={`text-gray-600 ${darkTheme ? 'text-white' : 'text-gray-400'}`}
+//                 >
+//                   Year: {education.year}
+//                 </p>
+//                 <p
+//                   className={`text-gray-600 ${darkTheme ? 'text-white' : 'text-gray-400'}`}
+//                 >
+//                   Received From: {education.name}
+//                 </p>
+//               </div>
--- server/db/seeds/workHistory.js
+++ server/db/seeds/workHistory.js
@@ -7,30 +7,22 @@ export async function seed(knex) {
   await knex('workHistory').insert([
     {
       id: 1,
-      years: '2019-2021',
-      name: 'Auckland Adventure Park',
-      title: 'Ticket Sales and Ride Operator',
+      years: '2024',
+      name: 'Dev Academy Aotearoa',
+      title: 'Full Stack Developer (Student)',
       description:
-        'Focusing on customer service, I mainly spent time in reception, where I sold tickets at the kiosk and helped with customer queries. When I was not doing this, I would be working as a ride operator to ensure health and safety was the highest priority.',
+        'Throughout my time at Dev Academy, I have come to appreciate the idea of continued development and learning strategies. Some of the useful tools and languages I have learnt and practiced include: JavaScript, TypeScript, Git, React, Node.js, SQLite3 database creation and database management. I’ve also been introduced to basic authentication with Auth0, external API’s, front-end testing and back-end testing. A large focus of this course is also the development of soft-skills such as communication, receiving feedback, etc. And this is enforced with completing multiple group projects.',
     },
     {
       id: 2,
-      years: '2021-2022',
-      name: 'Legacy Nutrition and Fitness',
-      title: 'Personal Trainer and Salesperson',
+      years: '2023',
+      name: 'Red Energy Australia',
+      title: 'Customer Initialisation Specialist',
       description:
-        'Here, I worked as a personal trainer in a studio, building relationships and helping clients to achieve their goals. When I was not doing this, I was helping the store to sell supplements in a customer-facing role.',
+        "I was only in this role for about 2-3 months (Melbourne based) before needing to return home to NZ due to family illness. My role's function was to read a script to our customers to gain their explicit and informed consent for signing up with Red. When beginning the role, it involved learning a lot of new systems/applications that the company used, new processes, inductions, etc. The main focus and enjoyable part was the team culture and talking to awesome customers all day. It was a wonderful experience to speak with customers all day from a call centre as well as learn from such an amazing company.",
     },
     {
       id: 3,
-      years: '2022-2023',
-      name: 'Seeka Limited',
-      title: 'Services and Support',
-      description:
-        'This was an administrative role. It included co-ordinating projects and activities, dispatching, working as part of a call centre and organisation. It also involved a lot of collaboration, communication, reporting to the assistant manager, rostering staff and assisting with payroll at a basic level. I also assisted with asset management of company cars and phones.',
-    },
-    {
-      id: 4,
       years: '2022-2023',
       name: 'Odd Jobz TGA',
       title: 'Administration and Handy Person',
@@ -38,12 +30,28 @@ export async function seed(knex) {
         'I worked part-time/weekends as an administratorfor this company. My duties included liaising with customers, making appointments, notifying the owner of his appointments, giving quotes to customers, managing social media accounts and more. I also took some shifts to help with house moving, gardening, etc. (Any odd jobs that people needed done).',
     },
     {
-      id: 5,
-      years: '2023',
-      name: 'Red Energy Australia',
-      title: 'Customer Initialisation Specialist',
+      id: 4,
+      years: '2022-2023',
+      name: 'Seeka Limited',
+      title: 'Services and Support',
       description:
-        'I was only in this role for about 2-3 months (Melbourne based) before needing to return home to NZ due to family illness. My role&apos;s function was to read a script to our customers to gain their explicit and informed consent for signing up with Red. When beginning the role, it involved learning a lot of new systems/applications that the company used, new processes, inductions, etc. The main focus and enjoyable part was the team culture and talking to awesome customers all day. It was a wonderful experience to speak with customers all day from a call centre as well as learn from such an amazing company.',
+        'This was an administrative role. It included co-ordinating projects and activities, dispatching, working as part of a call centre and organisation. It also involved a lot of collaboration, communication, reporting to the assistant manager, rostering staff and assisting with payroll at a basic level. I also assisted with asset management of company cars and phones.',
+    },
+    {
+      id: 5,
+      years: '2021-2022',
+      name: 'Legacy Nutrition and Fitness',
+      title: 'Personal Trainer and Salesperson',
+      description:
+        "Here, I was self-employed and worked as a personal trainer in a studio, building relationships and helping my clients achieve their goals. When I wasn't doing this, I was helping the store to sell supplements in a customer-facing role. There was also a lot of picking and packing of orders Involved. I learnt a lot about social media marketing, in-person and online sales, as well as time management, basic payroll functions and more. Reason for leaving: company went solely online and subsequently closed.",
+    },
+    {
+      id: 6,
+      years: '2019-2021',
+      name: 'Auckland Adventure Park',
+      title: 'Ticket Sales and Ride Operator',
+      description:
+        'Focusing on customer service, I mainly spent time in reception, where I sold tickets at the kiosk and helped with customer queries. When I was not doing this, I would be working as a ride operator to ensure health and safety was the highest priority.',
     },
   ])
 }
